55fa20e41d97bf67b3477d493f336f6f21056e4d
John Soares <esoajoh@elx5w5pw02-39.(none)>
2015-04-13 16:27:30 +0100

    Fix scritp for simulcript

--- test/test_simulcrypt.sh
+++ test/test_simulcrypt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python2 ../src/CodeDependencyVisualizer.py -aiptP --verbose -d "${RES_DIR}/Software/Resources/Simulcrypt/ECMSubsystem" -I "${RES_DIR}/Software/" && dot -T svg -o uml.svg uml.dot && firefox uml.svg
+python2 ../src/CodeDependencyVisualizer.py -aiptP --verbose -d ${RES_DIR}/Resources/Simulcrypt/ECMSubsystem -I ${RES_DIR}/ && dot -T svg -o uml.svg uml.dot && firefox uml.svg
